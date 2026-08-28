b69d62c8dd9f5c1dba0eedacc6b02f264e5667b6
Gircsi117 <gilianerik1107@gmail.com>
2026-08-28 17:08:49 +0200

    Add initial implementation for special Pythagorean triplet problem

---
+++ src/9_Task/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+/*
+Speciális Pitagoraszi-hármas
+
+A Pitagoraszi-hármas három természetes szám halmaza, a < b < c, amelyekre teljesül:
+
+a² + b² = c².
+
+Például 3² + 4² = 9 + 16 = 25 = 5².
+
+Pontosan egy olyan Pitagoraszi-hármas létezik, amelyre a + b + c = 1000.
+Határozd meg az abc szorzatot.
+*/
+
+int main()
+{
+  cout << "Hello World!";
+  return 0;
+}
