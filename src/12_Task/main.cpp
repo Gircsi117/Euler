ab4fb3db771bfe99348d74f49b6ea11ec94f0a63
Gilián Erik <gilianerik1107@gmail.com>
2026-08-31 14:38:36 +0200

    Add initial implementation for triangle number problem with placeholder output

---
+++ src/12_Task/main.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+/*
+A háromszögszámok sorozata a természetes számok összeadásával jön létre. Tehát a 7. háromszögszám
+
+1+2+3+4+5+6+7=28 lenne. Az első tíz tag a következő lenne:
+
+1,3,6,10,15,21,28,36,45,55,…
+
+Soroljuk fel az első hét háromszögszám osztóit:
+
+1   : 1
+3   : 1, 3
+6   : 1, 2, 3, 6
+10  : 1, 2, 5, 10
+15  : 1, 3, 5, 15
+21  : 1, 3, 7, 21
+28  : 1, 2, 4, 7, 14, 28
+
+Láthatjuk, hogy 28 az első háromszögszám, aminek több mint öt osztója van.
+
+Mennyi az első háromszögszám értéke, aminek több mint ötszáz osztója van?
+*/
+
+const int COUNT = 500;
+
+int main()
+{
+  
+
+
+  cout << "Hello World!";
+  return 0;
+}
