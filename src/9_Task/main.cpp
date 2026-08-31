c7314139134beafcb423300077238e5b4b310ae8
Gilián Erik <gilianerik1107@gmail.com>
2026-08-31 10:53:18 +0200

    Implement special Pythagorean triplet calculation with detailed output

--- src/9_Task/main.cpp
+++ src/9_Task/main.cpp
@@ -12,10 +12,56 @@ Például 3² + 4² = 9 + 16 = 25 = 5².
 
 Pontosan egy olyan Pitagoraszi-hármas létezik, amelyre a + b + c = 1000.
 Határozd meg az abc szorzatot.
+
+a = m² - n²
+b = 2mn
+c = m² + n²
+
+a + b + c = 1000
+
+(m² - n²) + 2mn + (m² + n²) = 1000
+m² - n² + 2mn + m² + n² = 1000
+2mn + 2m²
+
+2m * (n + m) = 1000
+m * (m + n) = 500
 */
 
 int main()
 {
-  cout << "Hello World!";
+  int m = 1;
+  int n = 1;
+  int result = 0;
+
+  while(true)
+  {
+    result = m * (m + n);
+    cout << "------------------------\n";
+    cout << "m = " << m << "\n";
+    cout << "n = " << n << "\n";
+    cout << "m * (m + n) = " << result << "\n";
+
+    if(result == 500)
+    {
+      int a = (m * m) - (n * n);
+      int b = 2 * m * n;
+      int c = (m * m) + (n * n);
+
+      if(a + b + c == 1000 && a > 0 && b > 0 && c > 0){
+        cout << a << " + " << b << " + " << c << " = 1000\n";
+        cout << "Result: " << a * b * c;
+        break;
+      }
+    }
+
+    n++;
+    if(result > 500)
+    {
+      cout << "a\n";
+      n = 1;
+      m++;
+    }
+  }
+
   return 0;
 }
