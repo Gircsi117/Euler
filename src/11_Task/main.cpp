389c9e513612ab8fbec2548854f1f93f2cd15eb9
Gilián Erik <gilianerik1107@gmail.com>
2026-08-31 11:21:06 +0200

    Add initial implementation for 20x20 grid problem with placeholder output

---
+++ src/11_Task/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+/*
+A lenti 20×20-as rácsban négy szám be van jelölve pirossal, egy átlós vonal mentén.
+
+E számok szorzata 26 × 63 × 78 × 14 = 1788696.
+
+Mekkora a legnagyobb szorzata négy szomszédos számnak ugyanabban az irányban (fel, le, balra, jobbra vagy átlósan) a 20×20-as rácsban?
+*/
+
+int main()
+{
+  cout << "Hello World!";
+  return 0;
+}
