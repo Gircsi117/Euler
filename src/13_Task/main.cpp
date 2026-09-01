fb97fb4f77cf9352b9721f60c27c2473c0f1ac65
Gilián Erik <gilianerik1107@gmail.com>
2026-09-01 12:12:08 +0200

    Implement summation of 100 fifty-digit numbers and output the first ten digits

--- src/13_Task/main.cpp
+++ src/13_Task/main.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <fstream>
+#include <vector>
 using namespace std;
 
 /*
@@ -7,8 +9,58 @@ Az első tíz számjegyét kell kiszámolni a következő száz darab 50 jegyű 
 Írásbeli összeadás
 */
 
+const int NUMBER_LENGTH = 50;
+
 int main()
 {
-  cout << "Hello World!";
+  string myText;
+  ifstream MyReadFile("src/13_Task/data.txt");
+
+  vector<string> numbers = {};
+
+  while (getline(MyReadFile, myText))
+  {
+    numbers.push_back(myText);
+  }
+  MyReadFile.close();
+
+  string result = "";
+  int rest = 0;
+
+  for (int i = NUMBER_LENGTH - 1; i >= 0; i--)
+  {
+    int current = 0;
+
+    for (string number : numbers)
+    {
+      current += stoi(number.substr(i, 1));
+    }
+
+    current += rest;
+
+    string currentStr = to_string(current);
+
+    if (i == 0)
+    {
+      result = currentStr + result;
+      break;
+    }
+
+    string value = currentStr.substr(currentStr.length() - 1, 1);
+    result = value + result;
+
+    currentStr.pop_back();
+    if (currentStr.length() == 0)
+    {
+      rest = 0;
+    }
+    else
+    {
+      rest = stoi(currentStr);
+    }
+  }
+
+  cout << result << "\n";
+  cout << result.substr(0, 10) << "\n";
   return 0;
 }
