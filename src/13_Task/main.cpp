10e7997997c009fd77a24a74410091f0aafb6f5b
Gilián Erik <gilianerik1107@gmail.com>
2026-09-01 10:59:37 +0200

    Add initial implementation for summation of 100 fifty-digit numbers

---
+++ src/13_Task/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+/*
+Az első tíz számjegyét kell kiszámolni a következő száz darab 50 jegyű szám összegének.
+
+Írásbeli összeadás
+*/
+
+int main()
+{
+  cout << "Hello World!";
+  return 0;
+}
