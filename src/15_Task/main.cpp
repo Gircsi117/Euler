2a4c8351ab0fea1d1e69577735d53fa2cf354401
Gilián Erik <gilianerik1107@gmail.com>
2026-09-03 11:58:38 +0200

    Update grid size to 20 and enhance calculation for pathfinding in a 20x20 grid

--- src/15_Task/main.cpp
+++ src/15_Task/main.cpp
@@ -10,12 +10,31 @@ Hány ilyen útvonal van egy 20×20-as rácson keresztül?
 Megjegyzések:
 Egy n*n-es mezőben n-szer megyünk jobbra és n-szer le. A feladat, hogy ezeket hány féle képpen lehet egymás után helyezni.
 
+egy nxn-es területen n lépést mehetünk jobbra és n lépést mehetünk lefelé
+
 n alatt a k -> (n!)/(k! * (n-k)!)
+
+1 x 2 x 3 x 4 x 5 x 6 x 7 x 8 x 9 x 10
+---------------------------------------
+1 x 2 x 3 x 4 x 5 x (1 x 2 x 3 x 4 x 5)
+
+6 x 7 x 8 x 9 x 10
+------------------
+1 x 2 x 3 x 4 x 5
 */
 
-const int SIZE = 5;
+const int SIZE = 20;
 
 int main()
 {
+  double result = 1.0;
+
+  for (double i = 1; i <= SIZE; i++)
+  {
+    cout << SIZE + i << " / " << i << "\n";
+    result *= (SIZE + i) / i;
+  }
+
+  cout << "Result: " << to_string(result) << "\n";
   return 0;
 }
