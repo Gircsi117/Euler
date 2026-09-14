e0af1e70e1e6d1fee71d0ed58566c8f228925b98
Gilián Erik <gilianerik1107@gmail.com>
2026-09-14 16:30:31 +0200

    Add main.cpp for name scoring problem; includes initial setup and problem description

---
+++ src/22_Task/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+/*
+A data.txt fájl egy 46 KB méretű szövegfájl, amely több mint ötezer keresztnevet tartalmaz.
+Először rendezd ábécésorrendbe. Ezután számítsd ki minden név ábécé-értékét, és szorozd meg ezt az értéket a névnek a listában elfoglalt ábécé szerinti helyével,
+hogy megkapd a név pontszámát.
+
+Például: ábécésorrendbe rendezve a COLIN, aminek értéke 3 + 15 + 12 + 9 + 14 = 53, a lista 938. neve. Így a COLIN pontszáma 938 × 53 = 49714.
+
+Mennyi az összes névpontszám összege a fájlban?
+*/
+
+int main()
+{
+  cout << "Hello World!";
+  return 0;
+}
