1a6f66558f7f7939f2f1b0c8085aaa5e67e1b523
Gilián Erik <gilianerik1107@gmail.com>
2026-09-15 14:16:22 +0200

    Refactor comments in main.cpp for clarity and readability

--- src/23_Task/main.cpp
+++ src/23_Task/main.cpp
@@ -2,13 +2,15 @@
 using namespace std;
 
 /*
-Egy szám tökéletes szám, ha valódi osztóinak összege pontosan egyenlő magával a számmal. Például a 28 valódi osztóinak összege 1 + 2 + 4 + 7 + 14 = 28, tehát a 28 tökéletes szám.
+Egy szám tökéletes szám, ha valódi osztóinak összege pontosan egyenlő magával a számmal.
+Például a 28 valódi osztóinak összege 1 + 2 + 4 + 7 + 14 = 28, tehát a 28 tökéletes szám.
 
 Egy n számot hiányosnak nevezünk, ha valódi osztóinak összege kisebb, mint n, és bővelkedőnek, ha ez az összeg meghaladja n-et.
 
 Mivel 12 a legkisebb bővelkedő szám (1 + 2 + 3 + 4 + 6 = 16), a legkisebb szám, amely felírható két bővelkedő szám összegeként, a 24.
 Matematikai elemzéssel kimutatható, hogy minden 28123-nál nagyobb egész szám felírható két bővelkedő szám összegeként.
-Ez a felső korlát azonban elemzéssel tovább nem csökkenthető, jóllehet ismert, hogy a legnagyobb szám, amely nem fejezhető ki két bővelkedő szám összegeként, ennél a korlátnál kisebb.
+Ez a felső korlát azonban elemzéssel tovább nem csökkenthető, jóllehet ismert, hogy a legnagyobb szám,
+amely nem fejezhető ki két bővelkedő szám összegeként, ennél a korlátnál kisebb.
 
 Határozd meg az összes olyan pozitív egész szám összegét, amely nem írható fel két bővelkedő szám összegeként.
 */
