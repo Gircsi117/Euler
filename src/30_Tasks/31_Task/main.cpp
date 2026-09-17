f6d0ef331816f27ae5a19a082cb05158e70c351c
Gilián Erik <gilianerik1107@gmail.com>
2026-09-17 14:27:50 +0200

    Add initial implementation for task 31; set up main function and include necessary headers

---
+++ src/30_Tasks/31_Task/main.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
+#include <vector>
+#include <set>
+#include <map>
+#include <algorithm>
+#include <cmath>
+
+#include "../../common/math.h"
+#include "../../common/primes.h"
+#include "../../common/sort.h"
+#include "../../common/string.h"
+
+using namespace std;
+
+/*
+Az Egyesült Királyságban a pénznem font (£) és pennyből (p) áll. Nyolc érme van forgalomban:
+
+1p, 2p, 5p, 10p, 20p, 50p, £1 (100p) és £2 (200p).
+
+Lehetséges 2 fontot a következőképpen kirakni:
+
+1×£1 + 1×50p + 2×20p + 1×5p + 1×2p + 3×1p
+
+Hányféleképpen lehet kirakni 2 fontot bármennyi érme felhasználásával?
+*/
+
+int main()
+{
+  cout << "Result: " << "0" << std::endl;
+  return 0;
+}
