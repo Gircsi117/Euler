a3cc55eac497807a387d5f2156875a71df719408
Gilián Erik <gilianerik1107@gmail.com>
2026-09-21 15:34:57 +0200

    Add initial implementation for task 34; set up main function and include necessary headers

---
+++ src/30_Tasks/34_Task/main.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
+#include <vector>
+#include <set>
+#include <map>
+#include <algorithm>
+#include <cmath>
+
+#include "../../common/math.h"
+#include "../../common/primes.h"
+#include "../../common/sort.h"
+#include "../../common/string.h"
+
+using namespace std;
+
+/*
+145 egy furcsa szám, mivel 1! + 4! + 5! = 1 + 24 + 120 = 145.
+
+Határozd meg az összes olyan szám összegét, amely egyenlő a számjegyei faktoriálisainak összegével.
+
+Megjegyzés: Mivel 1! = 1 és 2! = 2 nem összegek, ezért ezek nem tartoznak bele.
+*/
+
+int main()
+{
+  cout << "Result: " << "0" << std::endl;
+  return 0;
+}
